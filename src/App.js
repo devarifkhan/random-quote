630493acda0dc6ebff8dae62447f4d3f16994070
devarifkhan <arifcse209@gmail.com>
2022-08-04 23:43:39 +0600

    Deployed Website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faQuoteLeftAlt } from '@fortawesome/free-solid-svg-icons';
+import { faTwitterSquare } from '@fortawesome/free-brands-svg-icons';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: [],
+      advice: '',
+    };
+  }
+
+  componentDidMount() {
+    this.handleclick();
+  }
+
+   handleclick() {
+    fetch("https:api.quotable.io/random")
+    .then(res => res.json())
+    .then(
+      (result) => {
+        this.setState({
+          isLoaded: true,
+          items: result
+        });
+      },
+      (error) => {
+        this.setState({
+          isLoaded: true,
+          error
+        })
+      }
+    )
+   }
+
+
+
+
+
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div id='quote-box'>
+
+          <h1 id='text'><span className='quotesDesign'><FontAwesomeIcon icon={faQuoteLeftAlt} /></span>{items.content}</h1>
+          <p id='author'>- {items.author}</p>
+          <div id='end'>
+            <div>
+              <button id='new-quote' onClick={this.handleclick.bind(this)}>New Quote</button>
+            </div>
+            <div>
+              <a id='tweet-quote' href={`https://twitter.com/intent/tweet?text=${items.content} - ${items.author}`} target='_blank' rel='noopener noreferrer'>
+                <FontAwesomeIcon icon={faTwitterSquare} />
+              </a>
+            </div>
+          </div>
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
